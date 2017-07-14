bf9fd025d501504633d8d916d74f348ba39089d8
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-07-14 19:14:49 -0300

    creating an enemy with random y position

--- demo.js
+++ demo.js
@@ -3,42 +3,67 @@ window.onload = function() {
   ctx = canv.getContext("2d");
   document.addEventListener("keydown", keyPush);
   setInterval(game, 1000 / 15);
+  createEnemies();
 };
 
 //main char position
 var px = 10;
 var py = 10;
 var gs = 20; //grid size
-var tc = 20; //tile counting
-var xv = 0; //x velocty
-var yv = 0;// y velocity
+var xv = 0; //x velocity
+var yv = 0; // y velocity
+var ex = 20; //x enemy position
+var ey = 0; //y enemy position
+
+
 
 function game() {
+
   px = px + xv;
   py = py + yv;
+
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canv.width, canv.height);
 
+  //plotting the main char
   ctx.fillStyle = "white";
-  ctx.fillRect(px*gs, py*gs, gs-2, gs-2);
+  ctx.fillRect(px * gs, py * gs, gs - 2, gs - 2);
+
+  moveEnemy();
+
 }
 
 function keyPush(evt) {
 
   switch (evt.keyCode) {
     case 37:
-        console.log("37");
-        xv=-1;yv=0;
-        break;
+      xv = -1;
+      yv = 0;
+      break;
     case 38:
-        xv=0;yv=-1;
-        break;
+      xv = 0;
+      yv = -1;
+      break;
     case 39:
-        xv=1;yv=0;
-        break;
+      xv = 1;
+      yv = 0;
+      break;
     case 40:
-        xv=0;yv=1;
-        break;
+      xv = 0;
+      yv = 1;
+      break;
   }
 
+}
+
+function createEnemies() {
+
+    ey = (Math.random() * 20);
+
+}
+
+function moveEnemy(){
+  ctx.fillStyle = "red";
+  ex = ex - 1;  
+  ctx.fillRect(ex*gs , ey * gs, gs - 2, gs - 2);
 }
